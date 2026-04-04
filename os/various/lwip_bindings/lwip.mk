bugobliterator/ChibiOS-1

lwip_bindings: add IPV6 functions as well

os/various/lwip_bindings/lwip.mk
@@ -10,7 +10,7 @@ LWBINDSRC = \
 
 
 # Add blocks of files from Filelists.mk as required for enabled options
-LWSRC_REQUIRED = $(COREFILES) $(CORE4FILES) $(APIFILES) $(LWBINDSRC) $(NETIFFILES)
+LWSRC_REQUIRED = $(COREFILES) $(CORE4FILES) $(CORE6FILES)  $(APIFILES) $(LWBINDSRC) $(NETIFFILES)
 LWSRC_EXTRAS ?= $(HTTPFILES)
 
 LWINC = \

andyp1per/ChibiOS

os/various/lwip_bindings/lwip.mk
@@ -11,7 +11,7 @@ LWBINDSRC = \
 
 
 # Add blocks of files from Filelists.mk as required for enabled options
-LWSRC_REQUIRED = $(COREFILES) $(CORE4FILES) $(APIFILES) $(LWBINDSRC) $(NETIFFILES)
+LWSRC_REQUIRED = $(COREFILES) $(CORE6FILES) $(CORE4FILES) $(APIFILES) $(LWBINDSRC) $(NETIFFILES)
 LWSRC_EXTRAS ?= $(HTTPFILES)
 
 LWINC = \